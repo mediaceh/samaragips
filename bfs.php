68e49c420c247bea44ed5a071e91569b4af4091a
Maks <immurz@gmail.com>
2019-08-13 02:03:45 +0400

    Update bfs.php

--- bfs.php
+++ bfs.php
@@ -24,7 +24,6 @@ function breadthFirstSearch(int $origin,int $destination): ?SplDoublyLinkedList 
     while (!$q->isEmpty() && $q->bottom() != $destination) {
         $t = $q->dequeue();
         if (!empty($graph[$t])) {
-            // для каждого соседнего узла
             foreach ($graph[$t] as $vertex) {
                 if (!$visited[$vertex]) {
                     $q->enqueue($vertex);
@@ -42,33 +41,31 @@ function breadthFirstSearch(int $origin,int $destination): ?SplDoublyLinkedList 
     }
 }
 
-
-fscanf(STDIN, "%d %d %d",
-    $N, // the total number of nodes in the level, including the gateways
-    $L, // the number of links
-    $E // the number of exit gateways
-);
 $graph = [];
+$exits = [];
+fscanf(STDIN, "%d %d %d",
+    $N, # the total number of nodes in the level, including the gateways
+    $L, # the number of links
+    $E  # the number of exit gateways
+);
 for ($i = 0; $i < $L; $i++) {
     fscanf(STDIN, "%d %d",
-        $N1, // N1 and N2 defines a link between these nodes
+        $N1, # N1 and N2 defines a link between these nodes
         $N2
     );
     $graph[$N1][] = $N2;
     $graph[$N2][] = $N1;
 }
-$exits = [];
 for ($i = 0; $i < $E; $i++) {
     fscanf(STDIN, "%d",
-        $EI // the index of a gateway node
+        $EI # the index of a gateway node
     );
     $exits[] = $EI;
 }
-// game loop
-while (TRUE)
-{
+# game loop
+while (TRUE) {
     fscanf(STDIN, "%d",
-        $SI // The index of the node on which the Skynet agent is positioned this turn
+        $SI # The index of the node on which the Skynet agent is positioned this turn
     );
     $path = null;
     foreach($exits as $exit) {
@@ -78,15 +75,13 @@ while (TRUE)
             }
         }
     }
-    // Write an action using echo(). DON'T FORGET THE TRAILING \n
-    // To debug (equivalent to var_dump): error_log(var_export($var, true));
+    # Write an action using echo(). DON'T FORGET THE TRAILING \n
+    # To debug (equivalent to var_dump): error_log(var_export($var, true));
     $B1 = $path->shift();
     $B2 = $path->shift();
     unset($graph[$B1][$B2]);
     unset($graph[$B2][$B1]);
-    // Example: 0 1 are the indices of the nodes you wish to sever the link between
+    # Example: 0 1 are the indices of the nodes you wish to sever the link between
     echo($B1.' '.$B2."\n");
-    
- 
 }
 ?>
